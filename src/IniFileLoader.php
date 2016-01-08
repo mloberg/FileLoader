18cb74b3c8777bab4825a14faaef63011f59e502
Matthew Loberg <loberg.matt@gmail.com>
2016-01-07 23:21:48 -0600

    Add IniFileLoader
    [skip ci]

---
+++ src/IniFileLoader.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * Copyright (c) 2015 Matthew Loberg
+ * Distributed under the MIT License (http://opensource.org/licenses/MIT)
+ */
+
+namespace Mlo\FileLoader;
+
+use Symfony\Component\Config\Loader\FileLoader as BaseFileLoader;
+
+/**
+ * IniFileLoader
+ *
+ * @author Matthew Loberg <loberg.matt@gmail.com>
+ */
+class IniFileLoader extends BaseFileLoader
+{
+    /**
+     * @inheritDoc
+     */
+    public function load($resource, $type = null)
+    {
+        return parse_ini_file($resource);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function supports($resource, $type = null)
+    {
+        return is_string($resource) && 'ini' === pathinfo($resource, PATHINFO_EXTENSION);
+    }
+}
